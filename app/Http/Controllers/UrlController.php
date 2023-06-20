68c0b9d88750567681d3f00dac9712b73f454ddf
angella6969 <irvan_arifafandi@yahoo.com>
2023-06-20 12:33:34 +0700

    first commit

---
+++ app/Http/Controllers/UrlController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\url;
+use App\Http\Requests\StoreurlRequest;
+use App\Http\Requests\UpdateurlRequest;
+
+class UrlController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreurlRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(url $url)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(url $url)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateurlRequest $request, url $url)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(url $url)
+    {
+        //
+    }
+}
